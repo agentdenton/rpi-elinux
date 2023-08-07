acc18339c04aa7687992e2dafb77d4beb115218c
Andy Brychenko <andybrychenko@gmail.com>
2023-08-07 12:26:54 +0300

    br2-external: rpi0w: remove unused parameters from the boot.cmd

--- buildroot/br2-external/board/rpi0w/boot.cmd
+++ buildroot/br2-external/board/rpi0w/boot.cmd
@@ -1,3 +1,3 @@
-setenv bootargs "8250.nr_uarts=1 root=/dev/mmcblk0p2 rootwait earlyprintk=serial,ttyS0,115200 console=tty1 console=ttyS0,115200 loglevel=7 printk.devkmsg=on blink.enable_module=1 bmp280.enable_module=1"
+setenv bootargs "8250.nr_uarts=1 root=/dev/mmcblk0p2 rootwait earlyprintk=serial,ttyS0,115200 console=tty1 console=ttyS0,115200 loglevel=7 printk.devkmsg=on"
 fatload mmc 0:1 ${kernel_addr_r} zImage
 bootz ${kernel_addr_r} - ${fdt_addr}
