8105da6ffeafb4c5fdcd3806ac35ee1bf9ba836f
Andy Brychenko <andybrychenko@gmail.com>
2023-12-25 16:16:27 +0200

    setup_docker: Configure access to the docker container on the local network
    1) Use macvlan driver and create a custom network for the docker container
    2) Create a custom macvlan bridge on the host device
    3) The ip addresses should be outside DHCP range of the router

--- setup_docker.sh
+++ setup_docker.sh
@@ -4,47 +4,33 @@ USERNAME="rpi"
 
 CONTAINER_NAME="rpi-elinux"
 CONTAINER_IMG_NAME="rpi-elinux-img"
+CONTAINER_NET_NAME="rpi-elinux-macvlan"
+
+SUBNET="192.168.50.0/24"
+GATEWAY="192.168.50.1"
+INTERFACE="enp4s0f4u1u1"
+HOST_MACVLAN="rpi-macvlan"
+
+CONTAINER_IP_ADDR="192.168.50.102"
+HOST_MACVLAN_IP_ADDR="192.168.50.103"
 
 MNT_DIR="/home/$USERNAME/rpi-dev"
 
-create_container() {
-    # create the image
-    docker build -t $CONTAINER_IMG_NAME \
-    --build-arg USERNAME=$USERNAME \
-    --build-arg WORKDIR=$MNT_DIR .
-
-    # create the container
-    docker create -it --privileged \
-        --mount type=bind,source="$PWD",target=$MNT_DIR \
-        --name $CONTAINER_NAME $CONTAINER_IMG_NAME
-}
-
 remove_container() {
     # remove previously created images
-    if [[ -n $(docker ps -a | grep "$CONTAINER_NAME") ]]; then
+    if docker ps -a | grep $CONTAINER_NAME; then
         # Check if container is running before stopping
-        if [[ -n $(docker ps -q -f name=$CONTAINER_NAME) ]]; then
-            docker stop "$CONTAINER_NAME"
+        if docker ps -q -f name=$CONTAINER_NAME; then
+            docker stop $CONTAINER_NAME
         fi
-        docker rm "$CONTAINER_NAME" || true
-        docker rmi "$CONTAINER_IMG_NAME" || true
+        docker rm $CONTAINER_NAME || true
+        docker rmi $CONTAINER_IMG_NAME || true
     fi
-}
 
-setup_container() {
-    remove_container # Remove the previous container before creating a new one.
-    create_container
-
-    # START
-    docker start $CONTAINER_NAME
-
-    # Copy github host ssh keys to the container
-    docker cp $HOME/.ssh/github $CONTAINER_NAME:/home/$USERNAME/.ssh
-    docker cp $HOME/.ssh/github.pub $CONTAINER_NAME:/home/$USERNAME/.ssh
-    docker cp $HOME/.ssh/known_hosts $CONTAINER_NAME:/home/$USERNAME/.ssh
-
-    # END
-    docker stop $CONTAINER_NAME
+    # Remove network if already exists
+    if docker network ls | grep -q $CONTAINER_NET_NAME; then
+        docker network rm $CONTAINER_NET_NAME
+    fi
 }
 
 trap_handler() {
@@ -52,4 +38,41 @@ trap_handler() {
 }
 trap "echo 'Stopping...'; trap_handler" INT ERR
 
-setup_container
+remove_container && docker network create \
+    --driver=macvlan \
+    --subnet=$SUBNET \
+    --gateway=$GATEWAY \
+    -o parent=$INTERFACE \
+    $CONTAINER_NET_NAME
+
+# create the image
+docker build \
+    -t $CONTAINER_IMG_NAME \
+    --build-arg USERNAME=$USERNAME \
+    --build-arg WORKDIR=$MNT_DIR .
+
+# Access the container from any device on the the local network using a custom
+# macvlan bridge interface
+# NOTE: How to access docker container from the host and vise versa
+# https://www.linuxtechi.com/create-use-macvlan-network-in-docker/
+# NOTE: The macvlan interface name shouldn't be too long:
+# https://github.com/JonathanCasey/mullvad-ubuntu/issues/3
+if ! ip link | grep -q $HOST_MACVLAN; then
+    sudo ip link add $HOST_MACVLAN link $INTERFACE type macvlan mode bridge
+    sudo ip addr add $HOST_MACVLAN_IP_ADDR dev $HOST_MACVLAN
+    sudo ip link set $HOST_MACVLAN up
+    sudo ip route add $SUBNET dev $HOST_MACVLAN
+fi
+
+docker run \
+    -dit \
+    --privileged \
+    --mount type=bind,source=$PWD,target=$MNT_DIR \
+    --network $CONTAINER_NET_NAME \
+    --ip $CONTAINER_IP_ADDR \
+    --name $CONTAINER_NAME $CONTAINER_IMG_NAME
+
+# Copy github host ssh keys to the container
+docker cp $HOME/.ssh/github $CONTAINER_NAME:/home/$USERNAME/.ssh
+docker cp $HOME/.ssh/github.pub $CONTAINER_NAME:/home/$USERNAME/.ssh
+docker cp $HOME/.ssh/known_hosts $CONTAINER_NAME:/home/$USERNAME/.ssh
